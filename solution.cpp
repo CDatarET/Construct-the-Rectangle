090029686773c192038b93ad1e7d8d484980e954
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-08-10 21:25:40 +0530

    Create solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        for(int i = (int)sqrt(area); i >= 1; i--){
+            if(area % i == 0){
+                return {area / i, i};
+            }
+        }
+
+        return {-1, -1};
+    }
+};
